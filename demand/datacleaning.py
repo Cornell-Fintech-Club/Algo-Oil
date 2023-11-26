27ae75061dadc1e6c10fe6481fe2144b52ba8207
Jordan <jsh323>
2023-11-25 21:52:35 -0500

    datacleaning for motor fuel consumption in the US yearly modeled visuals for us export vs motor fuel consumption

--- demand/datacleaning.py
+++ demand/datacleaning.py
@@ -1,10 +1,11 @@
 import pandas as pd
-import io as StringIO
+from io import StringIO
 
 df = pd.read_csv(
     "datademand/U.S._Exports_of_Finished_Petroleum_Products.csv"
 )
 df[["Month", "Year"]] = df["Month Year "].str.split(" ", expand=True)
+
 
 
 df.drop("Month Year ", axis=1, inplace=True)
@@ -14,8 +15,11 @@ df.to_csv("datademand/cleaned_US_exports.csv", sep=",")
 
 cols = ['Letters', 'Year', 'Gallons per Vehicle', 'Randint', 'Desc', 'Metric' ]
 motordf = pd.read_csv("datademand/MER_T01_08.csv", names = cols)
-motordf['Year'] = motordf["Year"].str[:4]
+print(motordf["Year"].dtype)
+motordf['Year'] = motordf["Year"]//100
+
 finalmotordf = motordf[["Year", "Gallons per Vehicle"]]
+finalmotordf = finalmotordf[pd.to_numeric(finalmotordf['Year'], errors= "coerce") >= 1981]
 print(finalmotordf)
 finalmotordf.to_csv("datademand/cleaned_US_Fuel_Consumption.csv")
 
---
+++ demand/factor-modeling/visuals.py
@@ -0,0 +1,35 @@
+import pandas as pd 
+import matplotlib.pyplot as plt 
+import numpy as np 
+from datetime import datetime
+
+us_exports_df = pd.read_csv("../datademand/cleaned_US_exports.csv")
+print(us_exports_df)
+
+us_motor_df = pd.read_csv("../datademand/cleaned_US_Fuel_Consumption.csv")
+print(us_motor_df)
+
+merged_data = pd.merge(us_exports_df, us_motor_df , on='Year')
+
+fig, ax1 = plt.subplots(figsize=(10, 6))
+
+
+ax1.plot(merged_data['Year'], merged_data['U.S. Exports of Finished Petroleum Products Thousand Barrels per Day'], color='blue', label='US Exports')
+ax1.set_xlabel('Year')
+ax1.set_ylabel('U.S. Exports (Thousand Barrels per Day)', color='blue')
+ax1.tick_params(axis='y', labelcolor='blue')
+ax1.grid(True)
+
+ax2 = ax1.twinx()
+ax2.plot(merged_data['Year'], merged_data['Gallons per Vehicle'], color='red', label='Gallons per Vehicle')
+ax2.set_ylabel('Gallons per Vehicle', color='red')
+ax2.tick_params(axis='y', labelcolor='red')
+
+lines_1, labels_1 = ax1.get_legend_handles_labels()
+lines_2, labels_2 = ax2.get_legend_handles_labels()
+lines = lines_1 + lines_2
+labels = labels_1 + labels_2
+ax1.legend(lines, labels, loc='upper left')
+
+plt.title('Comparison of US Exports and Gallons per Vehicle')
+plt.show()
