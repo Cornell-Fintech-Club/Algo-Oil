016ebb8c6254a73b54cd76e702d58973ed76dea2
vivianredpanda <vivianredpanda@gmail.com>
2023-11-26 12:24:40 -0500

    Created preliminary dataset for model

--- demand/datacleaning.py
+++ demand/datacleaning.py
@@ -1,25 +1,40 @@
 import pandas as pd
 from io import StringIO
 
-df = pd.read_csv(
-    "datademand/U.S._Exports_of_Finished_Petroleum_Products.csv"
-)
+months = {
+    "Jan": "01",
+    "Feb": "02",
+    "Mar": "03",
+    "Apr": "04",
+    "May": "05",
+    "Jun": "06",
+    "Jul": "07",
+    "Aug": "08",
+    "Sep": "09",
+    "Oct": "10",
+    "Nov": "11",
+    "Dec": "12",
+}
+
+df = pd.read_csv("datademand/U.S._Exports_of_Finished_Petroleum_Products.csv")
 df[["Month", "Year"]] = df["Month Year "].str.split(" ", expand=True)
 
 
-
 df.drop("Month Year ", axis=1, inplace=True)
+
+df["Month"] = df["Month"].map(months)
 print(df)
-df.to_csv("datademand/cleaned_US_exports.csv", sep=",")
+df.to_csv("datademand/cleaned_US_exports.csv", sep=",", index=False)
 
 
-cols = ['Letters', 'Year', 'Gallons per Vehicle', 'Randint', 'Desc', 'Metric' ]
-motordf = pd.read_csv("datademand/MER_T01_08.csv", names = cols)
+cols = ["Letters", "Year", "Gallons per Vehicle", "Randint", "Desc", "Metric"]
+motordf = pd.read_csv("datademand/MER_T01_08.csv", names=cols)
 print(motordf["Year"].dtype)
-motordf['Year'] = motordf["Year"]//100
+motordf["Year"] = motordf["Year"] // 100
 
 finalmotordf = motordf[["Year", "Gallons per Vehicle"]]
-finalmotordf = finalmotordf[pd.to_numeric(finalmotordf['Year'], errors= "coerce") >= 1981]
+finalmotordf = finalmotordf[
+    pd.to_numeric(finalmotordf["Year"], errors="coerce") >= 1981
+]
 print(finalmotordf)
 finalmotordf.to_csv("datademand/cleaned_US_Fuel_Consumption.csv")
-
