90c0680ec5483fbe182a65c51b51bd54488c07b6
Omisha <os226@cornell.edu>
2023-12-02 05:44:32 -0500

    Sentiment: changed to Pytorch, have run on data

--- sentiment/my-project/model.py
+++ sentiment/my-project/model.py
@@ -1,165 +1,150 @@
+import torch.nn as nn
+import torch
+from tqdm import tqdm
+import os
 import pandas as pd
-import numpy as np
-import tensorflow as tf
-from tensorflow.keras.preprocessing.text import Tokenizer
-from tensorflow.keras.preprocessing.sequence import pad_sequences
+import torchtext
+from torchtext.vocab import Vocab
+from torchtext.data.utils import get_tokenizer
 from sklearn.model_selection import train_test_split
-from tensorflow.keras.layers import Embedding, LSTM, Dense, Softmax
-from tensorflow.keras.models import Sequential
+from torch.utils.data import DataLoader
+import torch.nn.utils.rnn as rnn_utils
 
-class LSTM(tf.keras.Model):
-    def __init__(self, input_dim, output_dim, input_length):
-        # hid_dim, final_dim, num_layers
+# done?
+class LSTM(nn.Module):
+    def __init__(self, input_dim, embedding_dim, hid_dim, final_dim):
         super(LSTM, self).__init__()
 
-        # embedding layer
-        self.embedding_layer = Embedding(input_dim=vocab_size, output_dim=embedding_dim, input_length=max_length)
-
-        # LSTM layer
-        self.lstm_layer = LSTM(100)
-
-        # linear layer
-        self.dense_layer = Dense(units=64, activation='relu')
+        self.embedding_layer = nn.Embedding(input_dim, embedding_dim)
+        self.lstm_layer = nn.LSTM(input_size=embedding_dim, hidden_size=hid_dim, batch_first=True, bidirectional=False, num_layers=1)
+        self.linear_layer = nn.Linear(hid_dim, final_dim)
+        self.act = nn.ReLU()
 
     def forward(self, input):
-        # forward pass 
         x = self.embedding_layer(input)
-        x = self.lstm_layer(x)
-        x = self.dense_layer(x)
+        x, _ = self.lstm_layer(x)
+        x = self.linear_layer(x[:, -1, :])  
+        x = self.act(x)
+        return x
 
-        # softmax
-        output = model.compile(optimizer='adam', loss='categorical_crossentropy', metrics=['accuracy'])
-        # output = tf.nn.softmax(x, axis=-1)  
-
-        return output
-
+# plan: read from csv to df, split into train/test/val, tokenize & pad, return all with vocab size
 def format_dataloader(file_path_train, file_path_test):
     # load data
-    df1 = pd.read_csv('file_path_train.csv', engine='python')
-    df2 = pd.read_csv('file_path_test', engine='python')
+    df1 = pd.read_csv(file_path_train, engine='python')
+    df2 = pd.read_csv(file_path_test, engine='python')
 
-    # split train into text and labels
     X_train = df1["Title"] + " " + df1["Description"] # include both title and then description
     y_train = df1["Label"] # label encoding: 0 as bullish, 1 as bearish, 2 as neutral
-
-    # split test into text and labels
     X_test = df2["Title"] + " " + df2["Description"] # include both title and then description
     y_test = df2["Label"] # label encoding: 0 as bullish, 1 as bearish, 2 as neutral
 
     # split into test and val data
-    # X_train, other_x, y_train, other_y = train_test_split(X, Y, test_size=0.3, random_state=42)
-    X_test, X_val, y_test, y_val = train_test_split(other_x, other_y, test_size=0.33, random_state=42)
+    third = int(len(X_test) / 3)
+    X_val = X_test.head(third)
+    y_val = y_test.head(third)
+    X_test = X_test.tail(third * 2).reset_index(drop=True)
+    y_test = y_test.tail(third *2).reset_index(drop=True)
 
-    # tokenization of train
-    token = Tokenizer()
-    token.fit_on_texts(X_train)
-    X_train = token.texts_to_sequences(X_train)
+    # tokenize & pad continue
+    tokenizer = torchtext.data.utils.get_tokenizer("basic_english")
+    tokenized_data_train = [tokenizer(sentence) for sentence in X_train]
+    unique_tokens = set(token for seq in tokenized_data_train for token in seq)
+    vocab = {token: idx for idx, token in enumerate(unique_tokens)}
 
-    # observe lengths
-    # Average Length: 276.97566628041716
-    # Median Length: 279.0
-    # Max Length: 548
-    # sequence_lengths = [len(seq) for seq in X]
-    # print("Average Length:", np.mean(sequence_lengths))
-    # print("Median Length:", np.median(sequence_lengths))
-    # print("Max Length:", np.max(sequence_lengths))
+    X_train = [torch.tensor([vocab[token] for token in tokenized_sentence]) for tokenized_sentence in tokenized_data_train]
+    vocab['<unk>'] = len(vocab)
+    X_train = [torch.tensor([vocab.get(token, vocab['<unk>']) for token in tokenized_sentence]) for tokenized_sentence in tokenized_data_train]
+    X_train = rnn_utils.pad_sequence(X_train, batch_first=True)
 
-    # standardize length
-    max_length = 300
-    X_train = pad_sequences(X_train, maxlen=max_length, padding='post', truncating='post')
+    max_seq_length_train = max(len(seq) for seq in X_train)
+    padded_data_train = rnn_utils.pad_sequence([torch.tensor(seq) for seq in X_train], batch_first=True)
 
-    X_val = token.texts_to_sequences(X_val)
-    X_val = pad_sequences(X_val, maxlen=max_length, padding='post', truncating='post')
+    # input size
+    vocab_size = len(vocab)
 
-    X_test = token.texts_to_sequences(X_test)
-    X_test = pad_sequences(X_test, maxlen=max_length, padding='post', truncating='post')
+    # tokenize & pad continue
+    tokenized_data_test = [tokenizer(sentence) for sentence in X_test]
+    X_test = [torch.tensor([vocab.get(token, vocab['<unk>']) for token in tokenized_sentence]) for tokenized_sentence in tokenized_data_test]
+    X_test = rnn_utils.pad_sequence(X_test, batch_first=True)
+    
+    tokenized_data_val = [tokenizer(sentence) for sentence in X_val]
+    X_val = [torch.tensor([vocab.get(token, vocab['<unk>']) for token in tokenized_sentence]) for tokenized_sentence in tokenized_data_val]
+    X_val = rnn_utils.pad_sequence(X_val, batch_first=True)
 
-    return X_train, y_train, X_test, y_test, X_val, y_val, 
+    return X_train, y_train, X_test, y_test, X_val, y_val, vocab_size
 
 def train_model(
-    model, train_dataloader, val_dataloader, num_epochs, optimizer, loss_fn, file=None
+    model, train, val, num_epochs, optimizer, loss_fn, batch_size=32, file=None
 ):
+    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+    model.to(device)
 
     for epoch in range(num_epochs):
         print(f"Epoch {epoch}")
         total_loss = 0
+        model.train()
         total_batchs = 0
+        for batch in tqdm(train, desc="Training Batches"):
+            optimizer.zero_grad()
+            input = batch[0].to(device)
+            label = batch[1].to(device)
 
-        # training
-        for batch in tqdm(train_dataloader, desc="Training Batches"):
-            input = batch["input"]
-            label = batch["label"]
+            output = model(input)
+            loss = loss_fn(output, label)
+            total_loss += loss
+            total_batchs += 1
 
-            with tf.GradientTape() as tape:
-                output = model(input)
-                loss = loss_fn(output, label)
-
-            gradients = tape.gradient(loss, model.trainable_variables)
-            optimizer.apply_gradients(zip(gradients, model.trainable_variables))
-
-            total_loss += loss.numpy()
-            total_batches += 1
-        
+            loss.backward()
+            optimizer.step()
         print(f"Training Loss: {total_loss / total_batchs}")
 
         val_loss = 0
         total_batchs = 0
+        model.eval()
+        for batch in tqdm(val, desc="validation batches"):
+            input = batch[0].to(device)
+            label = batch[1].to(device)
 
-        # validation
-        for batch in tqdm(val_dataloader, desc="validation batches"):
-            input = batch["input"]
-            label = batch["label"]
-
-            output = model(input)            
-            loss = loss_fn(output, label)
-            val_loss += loss.numpy()
+            output = model(input)
+            loss = loss_fn(output, label) 
+            val_loss += loss.item()
             total_batchs += 1
 
         print(f"Validation Loss: {val_loss / total_batchs}")
-
     if file:
-        model.save(file)
+        path = os.path.join(os.getcwd(), "models", file)
+        model.save_pretrained(path)
 
     return model
 
-def eval_model(model, test_dataloader, loss_fn):
-    device = torch.device("cuda" if tf.config.experimental.list_physical_devices("GPU") else "cpu")
-    model.eval()
-    test_loss = 0
-    total_batchs = 0
-    outputs = []
-    for batch in tqdm(test_dataloader, desc="test batches"):
-        input = batch["input"]
-        label = batch["label"]
+def eval_model(model, test, loss_fn):
+  device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+  model.eval()
+  test_loss = 0
+  total_batchs = 0
+  outputs = []
+  for batch in tqdm(test, desc="test batches"):
+      input = batch[0].to(device)
+      label = batch[1].to(device)
 
-        inputs = tf.convert_to_tensor(inputs, dtype=tf.float32)
-        labels = tf.convert_to_tensor(labels, dtype=tf.float32)
-        inputs, labels = inputs.to(device), labels.to(device)
-
-        output = model(input)
-        loss = loss_fn(output, label)
-        test_loss += loss.numpy()
-        total_batchs += 1
-        outputs.extend(output.numpy())
-    print(f"Test Loss: {test_loss / total_batchs}")
-    return outputs, test_loss / total_batchs
-
+      output = model(input)
+      loss = loss_fn(output, label) 
+      test_loss += loss.item()
+      total_batchs += 1
+      outputs.append(output.item())
+  print(f"Test Loss: {test_loss / total_batchs}")
+  return outputs, test_loss / total_batchs
 
 def pred_model(model, input):
-    device = "cuda" if tf.config.experimental.list_physical_devices("GPU") else "cpu"
+    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
     model.eval()
-
-    input = tf.convert_to_tensor(input, dtype=tf.float32)
     input = input.to(device)
 
     output = model(input)
-    return output.numpy()
-
+    return output
 
 def save_model(model, path):
-    model.save(path)
-
+  torch.save(model, path)
 
 def load_model(path):
-    return tf.keras.models.load_model(path)
-
+  return torch.load(path)
--- sentiment/my-project/model_train.py
+++ sentiment/my-project/model_train.py
@@ -1,29 +1,37 @@
-from model import (
-    LSTM,
-    format_dataloader,
-    train_model,
-    pred_model,
-    save_model,
-    load_model,
-)
+import torch.optim as optim
+import torch
+from torch.utils.data import TensorDataset, DataLoader
 
 # parameters
 embedding_dim = 50
+hid_dim = 64
+final_dim = 3
 max_length = 300
 lr = 0.1
+num_epochs = 5
+batch_size=32
 
 # load data
-df1 = pd.read_csv('/Users/omishasharma/Downloads/atd_separate_reduced_annotated_train.csv', engine='python')
-df2 = pd.read_csv('/Users/omishasharma/Downloads/atd_separate_reduced_annotated_train.csv', engine='python')
+X_train, y_train, X_test, y_test, X_val, y_val, vocab_size = format_dataloader('sentiment/my-project/atd_separate_reduced_train_annotated.csv', 'sentiment/my-project/atd_separate_reduced_test_annotated.csv')
+train_dataset = TensorDataset(X_train, torch.tensor(y_train))
+train_dataloader = DataLoader(train_dataset, batch_size=batch_size, shuffle=True)
+test_dataset = TensorDataset(X_test, torch.tensor(y_test))
+test_dataloader = DataLoader(test_dataset, batch_size=batch_size, shuffle=True)
+val_dataset = TensorDataset(X_val, torch.tensor(y_val))
+val_dataloader = DataLoader(val_dataset, batch_size=batch_size, shuffle=True)
 
+# get input size
+input_dim = vocab_size
+
+# other definitions
 models = []
-loss_fn = tf.keras.losses.CategoricalCrossentropy()
+loss_fn = torch.nn.CrossEntropyLoss()
 preds = [0.0 for _ in range(10)]
 
+# model
 for n in range(5):
-    model = LSTM(vocab_size, embedding_dim, max_length)
-    optimizer = tf.keras.optimizers.SGD(learning_rate=lr)
-    # might need extra step to convert the X/y split to one data frame for both train & test & val?
-    train_model(model, train + val, test, num_epochs, optimizer, loss_fn)
+    model = LSTM(input_dim, embedding_dim, hid_dim, final_dim)
+    optimizer = optim.Adam(model.parameters(), lr=0.01)
+    train_model(model, train_dataloader, val_dataloader, num_epochs, optimizer, loss_fn)
 
-    save_model(model, f"models/oil_ffnn_{n}.pth")
+    save_model(model, f"/my-project/oil_lstm_{n}.pth") # /my-project
