c49264f5115440e03b8db97e72ff427a1f9e26f1
Jordan <99847889+jordannnhan@users.noreply.github.com>
2024-05-05 00:59:32 -0400

    data cleaning and dates and Y csv test

--- demand_new/test.py
+++ demand_new/test.py
@@ -8,30 +8,63 @@ import matplotlib.pyplot as plt
 
 
 X = pd.read_csv("USexportsofcrude.csv")
-X = X.transpose()
-X.columns = X.iloc[0]
-X = X.iloc[1:]
-X = X.reset_index()
 
-date = X["index"]
-date = pd.to_datetime(date, format="%Y")
-X = X.rename(columns={"index": "Year"})
-lessen = [col for col in X.columns if "OECD" in col]
-lessen.append("Year")
-X = X[lessen]
+
+
+
+X['Date'] = pd.to_datetime(X['Date'], format='%b %d, %Y')
+X['Year'] = X['Date'].dt.year
+X['Month'] = X['Date'].dt.month
+X = X.groupby(['Year', 'Month'])['Weekly U.S. Exports of Crude Oil  (Thousand Barrels per Day)'].mean().reset_index()
+X.rename(columns={'Weekly U.S. Exports of Crude Oil  (Thousand Barrels per Day)': 'Monthly Average Exports (Thousand Barrels per Day)'}, inplace=True)
 print(X)
 
-Y = pd.read_csv("finishedmotorgasoline.csv")
-Y[["Month", "Year"]] = Y["Month"].str.split(" ", expand=True)
-Y["Year"] = Y["Year"].astype(int)
-Y = (
-    Y.groupby("Year")["U.S. Field Production of Crude Oil Thousand Barrels"]
-    .sum()
-    .reset_index()
-)
 
-del Y["Year"]
+X['Date'] = pd.to_datetime(X['Year'].astype(int).astype(str) + '-' + X['Month'].astype(int).apply(lambda x: f'{x:02d}'))
+date = pd.DataFrame(X['Date'])
+print(date)
+
+del X['Date']
+
+# date = X["Date"]
+# date = pd.to_datetime(date)
+# print(date)
+
+
+
+
+# X['YearMonth'] = X['Date'].dt.to_period('M')
+# exportscrude = X.groupby('YearMonth')['Weekly U.S. Exports of Crude Oil  (Thousand Barrels per Day)'].mean().reset_index()
+# exportscrude['YearMonth'] = exportscrude['YearMonth'].dt.to_timestamp()
+# print(exportscrude)
+
+# X = X.transpose()
+# X.columns = X.iloc[0]
+# X = X.iloc[1:]
+# X = X.reset_index()
+
+
+
+
+Y = pd.read_csv("finishedmotorgasoline.csv")
+Y = pd.DataFrame(Y)
+Y = Y.drop('Date', axis=1)
+
+Y.to_csv("Y_data.csv")
+date.to_csv("Dates.csv")
+
 print(Y)
+
+# Y[["Month", "Year"]] = Y["Month"].str.split(" ", expand=True)
+# Y["Year"] = Y["Year"].astype(int)
+# Y = (
+#     Y.groupby("Year")["U.S. Field Production of Crude Oil Thousand Barrels"]
+#     .sum()
+#     .reset_index()
+# )
+
+# del Y["Year"]
+# print(Y)
 
 m, n = X.shape
 
@@ -56,6 +89,8 @@ date_test = date.loc[np.floor(m * size) + 1 :]
 lr = LinearRegression()
 
 # Train the model using the training sets
+print(X_train)
+print(Y_train)
 lr.fit(X_train, Y_train)
 predict_train = lr.predict(X_train)
 predict_test = lr.predict(X_test)
@@ -68,47 +103,48 @@ print("Residual sum of squares: %.2f" % np.mean((Y_predict - Y) ** 2))
 # Explained variance score: 1 is perfect prediction
 print("Variance score: %.2f" % lr.score(X, Y))
 
+print(len(date))
+print(len(Y))
+# plt.xticks(rotation=45)
+# plt.plot_date(
+#     date,
+#     Y,
+#     fmt="ko-",
+#     xdate=True,
+#     ydate=False,
+#     label="Real value",
+#     ms=3,
+# )
 
-plt.xticks(rotation=45)
-plt.plot_date(
-    date,
-    Y,
-    fmt="ko-",
-    xdate=True,
-    ydate=False,
-    label="Real value",
-    ms=3,
-)
+# print(type(lr.predict(X_train)))
+# plt.plot_date(
+#     date_train,
+#     predict_train,
+#     fmt="o-",
+#     xdate=True,
+#     ydate=False,
+#     label="Predicted train",
+#     color="#0074D9",
+#     ms=3,
+# )
 
-print(type(lr.predict(X_train)))
-plt.plot_date(
-    date_train,
-    predict_train,
-    fmt="o-",
-    xdate=True,
-    ydate=False,
-    label="Predicted train",
-    color="#0074D9",
-    ms=3,
-)
-
-plt.plot_date(
-    date_test,
-    predict_test,
-    fmt="o-",
-    xdate=True,
-    ydate=False,
-    label="Predicted test",
-    color="#228B22",
-    ms=3,
-)
+# plt.plot_date(
+#     date_test,
+#     predict_test,
+#     fmt="o-",
+#     xdate=True,
+#     ydate=False,
+#     label="Predicted test",
+#     color="#228B22",
+#     ms=3,
+# )
 
 
-plt.legend(loc="upper center")
-plt.ylabel("U.S. Field Production of Crude Oil Thousand Barrels")
-plt.title("Predicted US Supply of Crude Oil Based on OECD Supply")
-plt.grid()
-plt.show()
+# plt.legend(loc="upper center")
+# plt.ylabel("U.S. Field Production of Crude Oil Thousand Barrels")
+# plt.title("Predicted US Supply of Crude Oil Based on OECD Supply")
+# plt.grid()
+# plt.show()
 
 
 ################################################
